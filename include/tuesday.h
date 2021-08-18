f3ef3bb591a229b1298b322aa70984aa56aa4328
James Spencer <73702889+jamesspencer19@users.noreply.github.com>
2021-08-18 09:08:01 +0000

    Password Complexity breaks down the password and then prints each character type to console.

--- include/tuesday.h
+++ include/tuesday.h
@@ -1,5 +1,52 @@
+int passwordChecker(std::string password) {
+  //number of letters in the string
+  int letters = 0;
+  //number of numbers in the string
+  int numbers = 0;
+  //number of special characters in the string
+  int specialCharacters = 0;
+  //the password length
+  int passwordLength = password.length();
+
+  //loop that passes through each character in the string for comparison
+  for(int i = 0; i < passwordLength; i++){
+    //stores each character for comparison
+    char character = password[i];
+    //standard library comparison to an alphabetic character
+    if(std::isalpha(character)){
+      //increment the number of letters in the string
+      letters++;
+      //output that a letter has been found
+      std::cout<<"letter";
+    }
+    //standard library comparison to an numeric character
+    else if(std::isdigit(character)){
+      //increment the number of numbers in the string
+      numbers++;
+      //output that a number has been found
+      std::cout<<"numbers";
+    }
+    //else a special character has been found
+    else{
+      //increment the number of special characters in the string
+      specialCharacters++;
+      //output that a special character has been found
+      std::cout<<"special";
+    }
+  }
+  return 0;
+}
+
+
 void passwordComplexityChecker(void) {
-	std::cout << " - passwordComplexityChecker: not yet implemented\n\n";
+  //store the password as a string
+  std::string pass;
+  //ask the user to enter their password
+  std::cout<<"Password: ";
+  //stores the input as pass
+  std::cin>>pass;
+  //passes the password through the checker
+  passwordChecker(pass);
 }
 
 
