5486c7be7d4262535f7812260bcb90515b5457e0
James Spencer <73702889+jamesspencer19@users.noreply.github.com>
2021-08-19 13:10:40 +0000

    Primer 5: Complete

--- include/wednesday.h
+++ include/wednesday.h
@@ -1,18 +1,32 @@
 #include <fstream>
 
 void phoneDirectory(void) {
+  std::string name;
+  std::string number;
   std::string line;
   std::ifstream fileobject; 
-  int numberlines = 0;
+  int numberlines;
+  std::string search;
 
-  fileobject.open("phonebook.csv"); 
+  std::cout<<"Please enter a name or number to search: ";
+  getline(std::cin, search);
+
+  fileobject.open("phonebook.csv");
+  for (numberlines = 0; std::getline(fileobject, line); ++numberlines);
+  std::cout<<"Searching "<<numberlines<<" records...\n\n";
+  fileobject.close();
+
+  fileobject.open("phonebook.csv");
   while(!fileobject.eof()){
-    getline(fileobject, line);
-    std::cout<<line<<std::endl;
-    numberlines++;
+    getline(fileobject, name ,',');
+    getline(fileobject, number);
+    if(search == name || search == number){
+      std::cout<<"Contact details:\n"<<name<<", T:"<<number;
+      fileobject.close();
+      return;
+    }
   }
-  std::cout<<"Number of records: "<<numberlines;
-  // close the opened file.
+  std::cout<<"Sorry, no contact details found";
   fileobject.close();
 }
 
